bbb4b64cab0cf05e0fec25b941f0d6e6f014ddee
zhuohua <zhuohuawu0603@gmail.com>
2018-04-09 23:52:16 -0400

    00029: some changes

--- src/main/scala/com/zhuohuawu/examples/sparktwo/Main.scala
+++ src/main/scala/com/zhuohuawu/examples/sparktwo/Main.scala
@@ -1,10 +1,14 @@
 package com.zhuohuawu.examples.sparktwo
-import org.apache.log4j.Logger
+import com.zhuohuawu.examples.sparktwo.sparkour.SWorkingDataFrames
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
   val logger = Logger.getLogger(SparkSessionExample.getClass)
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  Logger.getLogger(SWorkingDataFrames.getClass).setLevel(Level.ERROR)
+
   val sparkConf = new SparkConf()
     .setMaster("local[*]")
     .setAppName("spark-testing-example")
