b2086265c37587da8a576198b4391bbd7ba03462
zhuohua <zhuohuawu0603@gmail.com>
2018-05-13 23:14:07 -0400

    test udaf functions

--- src/main/scala/com/zhuohuawu/examples/sparktwo/simple/UDAFTest.scala
+++ src/main/scala/com/zhuohuawu/examples/sparktwo/simple/UDAFTest.scala
@@ -1,7 +1,10 @@
 package com.zhuohuawu.examples.sparktwo.simple
 
 import org.apache.log4j.Logger
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.types._
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -46,6 +49,54 @@ object UDAFTest {
   }
 
   def testUDAF(): Unit = {
+
+    val spark = SparkSession.builder.
+      master("local")
+      .appName("example")
+      .getOrCreate()
+
+
+
+    class MyAvg extends UserDefinedAggregateFunction {
+      override def inputSchema: StructType = StructType(StructField("value", DoubleType) :: Nil)
+
+      override def bufferSchema: StructType = StructType(
+        StructField("count", LongType) ::
+        StructField("sum", DoubleType) :: Nil
+      )
+
+      //return type
+      override def dataType: DataType = DoubleType
+
+      override def deterministic: Boolean = true
+
+      override def initialize(buffer: MutableAggregationBuffer): Unit = {
+        buffer(0) = 0L
+        buffer(1) = 0.0
+      }
+
+      override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+        buffer(0) = buffer.getAs[Long](0) + 1
+        buffer(1) = buffer.getAs[Double](1) + input.getAs[Double](0)
+      }
+
+      override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+        buffer1(0) = buffer1.getAs[Long](0) + buffer2.getAs[Long](0)
+        buffer1(1) = buffer1.getAs[Long](1) + buffer2.getAs[Long](1)
+      }
+
+      override def evaluate(buffer: Row): Any = {
+        buffer.getDouble(1) / buffer.getLong(0)
+      }
+    }
+
+    val myAvg = new MyAvg
+    import spark.implicits._
+    spark.udf.register("ourAvg", myAvg)
+
+    import org.apache.spark.ml._
+
+    val lr = new LogisticRegressionWithLBFGS()
 
   }
 
