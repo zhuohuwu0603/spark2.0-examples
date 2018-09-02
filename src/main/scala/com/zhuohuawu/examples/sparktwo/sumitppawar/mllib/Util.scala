f9e1531cee5f3166ed21288594e668c69f0b627b
zhuohua <zhuohuawu0603@gmail.com>
2018-09-02 09:41:46 -0400

    add ml example for decision tree

---
+++ src/main/scala/com/zhuohuawu/examples/sparktwo/sumitppawar/mllib/Util.scala
@@ -0,0 +1,10 @@
+//package learn.apache.spark.learn.apache.spark.mllib
+//
+//import org.apache.spark.{SparkConf, SparkContext}
+//
+//object Util {
+//
+//  private val config = new SparkConf().setMaster("local").setAppName("SparkApplication")
+//  lazy  val sc: SparkContext = new SparkContext(config)
+//
+//}
